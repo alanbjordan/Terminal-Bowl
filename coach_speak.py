ff3704ebafc2e76e833d4e3a7478a9d3bfdd3b94
Fred Major <sgtfredmajor@gmail.com>
2020-01-25 07:05:24 -0500

    Inital add coach speak

---
+++ coach_speak.py
@@ -0,0 +1,21 @@
+
+coach_speaking = ['''Coach Griffin speaking to players: It's not the SIZE of the dog in the fight, but the \nsize of the FIGHT in the dog: now get out there and get that first down!''',
+'''Coach Lombardi speaking to players: WINNING is not everything--but making the \nEFFORT to win is: now let’s go win this game!''',
+'''Coach Holtz speaking to players: If what you did YESTERDAY seems big, you \nhaven't done anything TODAY. Now let’s win this Super Bowl!''',
+'''Coach Landry speaking to players: A CHAMPION is simply someone who did NOT \ngive up when they wanted to. Now go out there and get a first down!''',
+'''Coach Holtz speaking to players: The man who complains about the way the ball \nBOUNCES is likely the one who DROPPED it. Now let’s win this thing!''',
+'''Coach Ditka speaking to players: If you're not IN the parade, you WATCH the parade. \nThat's life. Son, go get me a touchdown, we can bring that trophy home!''',
+'''Coach Tomlin speaking to players: We don’t live in our fears, we live in our hopes. \nNow let’s win this thing!''',
+'''Coach Noll speaking to players: Good things happen to those who hustle. So, hustle up and get that first down!''',
+'''Coach Vermeil speaking to players: When you believe in yourself and the people you surround \nyourself with, you will win something really big someday. \nNow let’s go win this game!''']
+
+
+print(coach_speaking[0])
+print(coach_speaking[1])
+print(coach_speaking[2])
+print(coach_speaking[3])
+print(coach_speaking[4])
+print(coach_speaking[5])
+print(coach_speaking[6])
+print(coach_speaking[7])
+print(coach_speaking[8])
